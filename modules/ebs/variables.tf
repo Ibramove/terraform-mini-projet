4c80fe7b4bd3dce6756c220445e1f922e7dc5c4b
Ibramove <81634863+Ibramove@users.noreply.github.com>
2023-11-10 01:08:01 +0100

    Update variables.tf

--- modules/ebs/variables.tf
+++ modules/ebs/variables.tf
@@ -1,1 +1,9 @@
+variable "ebs_volume_size" {
+  description = "Taille du volume EBS (en Go)"
+  type        = number
+}
 
+variable "AZ" {
+  description = "Zone de disponibilité AWS où créer le volume EBS"
+  type        = string
+}
