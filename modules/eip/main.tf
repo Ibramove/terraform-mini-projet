05eeafe8dfc17e3cf992e8f8fa665e201e701826
Ibramove <81634863+Ibramove@users.noreply.github.com>
2023-11-10 01:09:03 +0100

    Update main.tf

--- modules/eip/main.tf
+++ modules/eip/main.tf
@@ -1,1 +1,8 @@
 
+
+#resource "aws_instance" "my_ip" {resource "aws_eip" "my_eip" {vpc = true}
+
+resource "aws_eip" "my_eip" {
+  domain = "vpc"  # Utilisation de "vpc" pour une EIP de domaine VPC.
+}
+
