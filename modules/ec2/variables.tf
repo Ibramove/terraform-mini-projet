87dd7c9ac2ac439e4bc6aa2cc0c76c2eb53363d3
Ibramove <81634863+Ibramove@users.noreply.github.com>
2023-11-10 01:06:49 +0100

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,52 @@
+variable "instance_type" {
+  description = "Type d'instance EC2"
+  type        = string
+  default     = "t2.nano"
+}
+variable "sg" {
+  type        = string
+  description = "set sg name "
+  default     = "ibra-sg"
+}
+
+variable "instance_name" {
+  description = "Nom de l'instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "root_volume_size" {
+  description = "Taille du volume racine de l'instance EC2 (en Go)"
+  type        = number
+  default     = 50
+}
+
+
+variable "ssh_key" {
+  description = "Nom de la clé SSH"
+  type        = string
+  default     = "devops"
+}
+
+variable "AZ" {
+  description = "Zone de disponibilité (Availability Zone)"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "Nom du groupe de sécurité"
+  type        = string
+  default     = "NULL"
+}
+variable "public_ip" {
+  description = "Adresse IP publique"
+  type        = string
+  default     = "NULL"
+}
+
+variable "user" {
+  description = "Nom d'utilisateur SSH"
+  type        = string
+  default     = "ubuntu"
+}
 
